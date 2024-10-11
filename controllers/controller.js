3e238f0c47b1cd7a1eb9646e76dadcffa870eb31
KEVIN CHAU <kevinch@udel.edu>
2024-10-11 00:01:10 -0400

    Changed UI and added dynamic data listing for movies and adding movie functionality

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,4 +1,23 @@
+const db = require("../db/query");
 
-exports.renderHomePage = (req, res) => {
-    res.render('homePage', {title: 'Homepage'})
+exports.renderHomePage = async (req, res) => {
+    const allMovies = await db.retrieveAllMovies();
+    res.render('homePage', {title: 'Homepage', movies: allMovies})
+}
+
+exports.submitMovie = async (req, res) => {
+    const {movie_title, movie_director, movie_genre, movie_main_actor, movie_released} = req.body;
+    const movieObj = {movie_title: movie_title, movie_director: movie_director, movie_genre: movie_genre, movie_main_actor: movie_main_actor, movie_released: movie_released};
+    await db.insertMovie(movieObj);
+    res.redirect('/');
+
+}
+
+exports.deleteAll = (req, res) => {
+    db.dropAllEntries();
+    res.send('All movies have been deleted!');
+}
+
+exports.renderAddPage = (req, res) => {
+    res.render('addMovie');
 }
