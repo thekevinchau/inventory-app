b10f5e217640f650d1e9bce659464e565c1bab34
KEVIN CHAU <kevinch@udel.edu>
2024-10-12 22:50:14 -0400

    Added http request to retrive image according to movie title

--- utils/testingFetch.js
+++ utils/testingFetch.js
@@ -1,13 +1,26 @@
-const url = 'https://api.themoviedb.org/3/search/movie?query=The%20Dark%20Knight&include_adult=false&language=en-US&page=1';
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNWI0OTk2MzAzYzY3NjMxOWNmMzFkNjQ2OTQzODExNiIsIm5iZiI6MTcyODc4MTkwNi4wNzcyNzgsInN1YiI6IjY3MGIxYWZlYmJiMWE5ZTgxYzYxYzkxZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KOsT_AyV-8CYKXrianBCzpJh9AWxxlZKpI5ExpWDmDE'
-  }
-};
+const path = require('path');
+require('dotenv').config({path: path.resolve(__dirname, '../.env')})
 
-fetch(url, options)
-  .then(res => res.json())
-  .then(json => console.log(json.results[0].poster_path))
-  .catch(err => console.error('error:' + err));
+const TMDB_API_TOKEN = process.env.TMDB_API_TOKEN;
+
+const retriveMovieImage = async(movie_title) => {
+  const url = `https://api.themoviedb.org/3/search/movie?query=${movie_title}&include_adult=false&language=en-US&page=1`;
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: TMDB_API_TOKEN
+    }
+  };
+
+  try{
+    const query = await fetch(url, options);
+    const response = await query.json();
+    console.log(response.results[0].poster_path);
+  }
+  catch(err){
+    console.log('Error fetching movie image', err);
+  }
+}
+
+module.exports = retriveMovieImage;
